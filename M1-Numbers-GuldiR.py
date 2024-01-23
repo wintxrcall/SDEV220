ce39011bec0aed01927d01cd5a467df4b5b136ab
Wintxr <108226679+wintxrcall@users.noreply.github.com>
2024-01-22 20:43:28 -0500

    M1: Numbers & Types
    module one assignment code following questions in textbook to assign variables

---
+++ M1-Numbers-GuldiR.py
@@ -0,0 +1,11 @@
+seconds_per_minute = 60
+minutes_per_hour = 60
+seconds_per_hour = seconds_per_minute * minutes_per_hour
+seconds_per_day = seconds_per_hour * 24
+flt_pt_div = seconds_per_day / seconds_per_hour
+intg_div = seconds_per_day // seconds_per_hour
+
+print(flt_pt_div)
+print(intg_div)
+
+print("3.6: \n Yes, the float point and integer division returned the same result.")
